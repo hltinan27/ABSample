1a16362e54531972eb53c9afe614450ec0adf8fa
Halit inan <halit.inan@aktifbank.com.tr>
2020-02-12 00:04:48 +0300

    Initial Commit

---
+++ ABSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ABSample
+//
+//  Created by Halit inan on 12.02.2020.
+//  Copyright © 2020 Halit inan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
